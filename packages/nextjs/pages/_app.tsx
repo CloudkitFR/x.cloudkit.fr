e993cfeaaddc86518feb9ea7710c58df6378d15e
Eiermann Patrick <eiermann.patrick.pro@outlook.com>
2021-12-16 10:23:05 +0100

    adding db

--- packages/nextjs/pages/_app.tsx
+++ packages/nextjs/pages/_app.tsx
@@ -1,5 +1,5 @@
 import '../styles/globals.css'
-import "xterm/css/xterm.css"
+import "../node_modules/xterm/css/xterm.css"
 import type { AppProps } from 'next/app'
 
 function MyApp({ Component, pageProps }: AppProps) {
